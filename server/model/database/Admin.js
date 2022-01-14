ea2182d5ead3ddebd450e3f644580fbfc35f73c4
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2022-01-14 20:36:20 +0530

    Delete server directory

--- server/model/database/Admin.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose"); 
-
-const userSchema = new mongoose.Schema(
-    { firstName: { type: String, required: true }, lastName: { type: String }, email: { type: String, required: true, unique: true }, education: { type: String, required: true }, contactNo: { type: String, required: true}, bookingtime: { type: Date, default: Date.now },exper:{type:String,required:true}})
-     
-    module.exports = mongoose.model("Admin", userSchema);
